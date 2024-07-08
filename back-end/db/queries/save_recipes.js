bfddd6ed4ba5587e7f7ec0a6075bf0ab70982e15
ihleung <ihfungleung@gmail.com>
2024-07-08 12:16:27 -0400

    add save recipe query

--- back-end/db/queries/save_recipes.js
+++ back-end/db/queries/save_recipes.js
@@ -1,2 +1,11 @@
 const db = require('../connection');
 
+const addSaveRecipe = function(user_id, recipe_id) {
+  return db.query(
+    'INSERT INTO saved_recipes (user_id, recipe_id) VALUES ($1, $2) RETURNING *;', [user_id, recipe_id])
+    .then(data => {
+      return data.rows[0];
+    }).catch((err) => {
+      console.log(err.message);
+    });
+}
