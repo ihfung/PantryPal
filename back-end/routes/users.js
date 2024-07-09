28e06f58abe7d70a6dd8ac7e6e6be7edb1a51c61
Arpi Patel <arpipatel96@gmail.com>
2024-07-09 15:33:23 -0400

    Add user routes (#8)
    * add routes to handle requests to controller functions
    
    * add routes
    
    ---------
    
    Co-authored-by: Gokul Krishna <gokursx.123@gmail.com>

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -1,3 +1,71 @@
 const express = require('express');
 const router = express.Router();
 const userQueries = require('../db/queries/users');
+
+router.get("/login", (req, res) => {
+  res.render('login', { user: req.session.userId });
+});
+
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  userQueries
+    .getUserByEmail(email)
+    .then((user) => {
+      if (!user) {
+        return res.send({ error: "no user with that email" });
+      }
+      console.log(user.password);
+      if (password !== user.password) {
+        return res.send({ error: "error" });
+      }
+
+      req.session.userId = user.id;
+      res.redirect("/main");
+    });
+});
+
+router.get('/register', (req, res) => {
+  res.render('register', { user: req.session.userId });
+});
+
+router.post("/register", (req, res) => {
+  const { name, email, password, phone } = req.body;
+  userQueries
+    .addUser(name, email, password, phone)
+    .then((user) => {
+      req.session.userId = user.id;
+      res.redirect("/main");
+    })
+    .catch((err) => res.send(err));
+});
+
+router.get("/", (req, res) => {
+  const userId = req.session.userId;
+  if (!userId) {
+    return res.send({ message: "not logged in" });
+  }
+
+  userQueries
+    .getUserById(userId)
+    .then((user) => {
+      if (!user) {
+        return res.send({ error: "no" });
+      }
+
+      res.send({
+        user: {
+          name: user.name,
+          email: user.email,
+          id: userId,
+        },
+      });
+    })
+    .catch((e) => res.send(e));
+});
+
+router.post("/logout", (req, res) => {
+  req.session.userId = null;
+  res.redirect("/users/login");
+});
+
+module.exports = router;
